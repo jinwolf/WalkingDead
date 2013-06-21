f462d3973268fd32613f437bcc3c0ab49ced0d27
Jin Lee <rosenwolf@gmail.com>
2013-06-20 18:22:26 -0700

    changed entity

--- game.js
+++ game.js
@@ -1,32 +1,15 @@
-   var factory = {};
+var factory = {};
 
-   var EntityClass = Class.extend({
-   // TASK #1
-   // Fill out the base class for game entities
-   // with the following properties:
-   // 
-   // 1) a 'pos' object with 'x' and 'y' properties
-   //    to keep track of the entity's position in
-   //    the game world.
-   // 
-   // 2) a 'size' object with 'x' and 'y' properties
-   //    to keep track of an entity's size for the
-   //    purpose of drawing.
-   // 
-   // 3) a blank update function we can overload to
-   //    update each entity in a specific manner.
-   //    This is what allows us to give each entity
-   //    type its own logic to perform each frame.
-   //
-   pos : {x:0,y:0},
-   size : {x:0,y:0},
-   last : {x:0,y:0},
+Entity = Class.extend({
+   init: function() {
+   },
 
-   update : function() { }
-   });
+   update: function() {
+   }
+});
 
-   var PlayerClass = EntityClass.extend({
+var Player = Entity.extend({
 
-   });
+});
 
-   factory['PlayerClass'] = PlayerClass;
+factory['Player'] = PlayerClass;
