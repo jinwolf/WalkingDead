b63abcdd6b19e91d74e31231401e37184fd17816
Edward Pearson <edpearson@yahoo.com>
2013-06-21 04:32:12 -0700

    UPdate

--- Player.js
+++ Player.js
@@ -4,113 +4,97 @@ var Player = Entity.extend({
         'left': 'left',
         'down': 'down',
         'right': 'right',
-         'punch': 'punch',
-         'shoot': 'shoot'
+        'punch': 'punch',
+        'shoot': 'shoot'
     },
-    /**
-   * Bitmap dimensions
-   */
+    
+	/**
+    * Bitmap dimensions
+    */
     size: {
-        w: 48,
-        h: 48
+        w: 136,
+        h: 136
     },
 
     alive: true,
-
     isStopped: false,
 
-        /**
-* Bitmap animation
-*/
+    /**
+	* Bitmap animation
+	*/
     bmp: null,
-
     body: null,
 
-   init: function(position, id) {
+	init: function(position, id) {
+		var img = gGameEngine.playerImg;
+		if (!(this instanceof Bot)) {
+			img = gGameEngine.playerImg;
+		}
 
+		if (id) {
+		    this.id = id;
+		}
 
-           var img = gGameEngine.playerImg;
-           if (!(this instanceof Bot)) {
-                   img = gGameEngine.playerImg;
-           }
+		var spriteSheet = new createjs.SpriteSheet({
+			images: [img],
+			frames: { width: this.size.w, height: this.size.h, count:6 },
+			animations: {
+				idle: [0, 0, 'idle'],
+				//down: [0, 3, 'down', 10],
+				left: [4, 5, 'left', 10],
+				//up: [8, 11, 'up', 10],
+				right: [0, 1, 'right', 10],
+				//dead: [16, 16, 'dead', 10]
+			}
+		});
+		
+		this.bmp = new createjs.BitmapAnimation(spriteSheet);
+		this.bmp.gotoAndPlay("right");
 
+		this.bmp.scaleY = 0.45;
+		this.bmp.scaleX = 0.45;
 
-      if (id) {
-         this.id = id;
-      }
+		b = this.bmp;
 
-      
-      var spriteSheet = new createjs.SpriteSheet({
-         images: [img],
-         frames: { width: this.size.w, height: this.size.h, regX: 10, regY: 12 },
-         animations: {
-             idle: [0, 0, 'idle'],
-             down: [0, 3, 'down', 10],
-             left: [4, 7, 'left', 10],
-             up: [8, 11, 'up', 10],
-             right: [12, 15, 'right', 10],
-             dead: [16, 16, 'dead', 10]
-         }
-      });
-      this.bmp = new createjs.BitmapAnimation(spriteSheet);
+		this.bmp.x = position.x;
+		this.bmp.y = position.y;
 
-      this.bmp.gotoAndPlay("right");
+		/////// CREATING 
+		this.createObj(this.bmp);
 
+		gGameEngine.stage.addChild(this.bmp);
+	},
 
-      this.bmp.scaleY = 0.8;
-      this.bmp.scaleX = 0.8;
+	createObj: function(skin)
+	{
+		var that = this;
 
-      b = this.bmp;
+        //create a ball
+		var fixDef = new b2FixtureDef;
+		fixDef.density = 0.5;
+		fixDef.friction = 0.5;
+		fixDef.restitution = 0;
 
-      this.bmp.x = position.x;
-      this.bmp.y = position.y;
+		var bodyDef = new b2BodyDef;
+		bodyDef.type = b2Body.b2_dynamicBody;
+		bodyDef.position.x = skin.x/gGameEngine.scale;
+		bodyDef.position.y = skin.y/gGameEngine.scale;
 
+		fixDef.shape = new b2PolygonShape;
+		fixDef.shape.SetAsBox(15/gGameEngine.scale, 27/gGameEngine.scale);
 
+		this.body = gGameEngine.world.CreateBody(bodyDef);
+		this.body.CreateFixture(fixDef);
 
+		// assign actor
+		var actor = gGameEngine.actorObject(that.body, skin);
 
-
-      /////// CREATING 
-      this.createObj(this.bmp);
-
-      gGameEngine.stage.addChild(this.bmp);
+		this.body.SetUserData(actor);  // set the actor as user data of the body so we can use it later: body.GetUserData()
+		//gGameEngine.bodies.push(that.body);
+		return that.body;
    },
 
-      createObj: function(skin)
-   {
-      console.log("skin", skin);
-      //return;
-      var that = this;
-               //create a ball
-         var fixDef = new b2FixtureDef;
-         fixDef.density = 0.5;
-         fixDef.friction = 0.5;
-         fixDef.restitution = 0;
-
-         var bodyDef = new b2BodyDef;
-         bodyDef.type = b2Body.b2_dynamicBody;
-         bodyDef.position.x = skin.x/gGameEngine.scale;
-         bodyDef.position.y = skin.y/gGameEngine.scale;
-
-
-         fixDef.shape = new b2PolygonShape;
-         fixDef.shape.SetAsBox(10/gGameEngine.scale, 11/gGameEngine.scale);
-
-         this.body = gGameEngine.world.CreateBody(bodyDef);
-         this.body.CreateFixture(fixDef);
-
-         // assign actor
-         var actor = gGameEngine.actorObject(that.body, skin);
-
-         this.body.SetUserData(actor);  // set the actor as user data of the body so we can use it later: body.GetUserData()
-         //gGameEngine.bodies.push(that.body);
-         return that.body;
-   },
-
-
-
-
-
-      update: function() {
+	update: function() {
         if (!this.alive) {
             //this.fade();
             return;
@@ -144,14 +128,14 @@ var Player = Entity.extend({
             that.vecX = 4;
             that.body.SetLinearVelocity(new b2Vec2(that.vecX,0));
 
-         } else if (gInputEngine.actions[this.controls.shoot]) {
+        } else if (gInputEngine.actions[this.controls.shoot]) {
             //this.animate('right');
             //position.x += this.velocity;
             //dirX = 1;
             that.vecX = -1;
             that.body.SetLinearVelocity(new b2Vec2(that.vecX,0));
 
-         } else if (gInputEngine.actions[this.controls.punch]) {
+        } else if (gInputEngine.actions[this.controls.punch]) {
             //this.animate('right');
             //position.x += this.velocity;
             //dirX = 1;
@@ -163,16 +147,16 @@ var Player = Entity.extend({
             that.body.SetLinearVelocity(new b2Vec2(0,0));
             that.isStopped = true;
         }
-      },
+    },
 
-       /**
-      * Changes animation if requested animation is not already current.
-      */
-       animate: function(animation) {
-           if (this.bmp.currentAnimation.indexOf(animation) === -1 || this.isStopped) {
-               this.bmp.gotoAndPlay(animation);
-           }
-       }
+	/**
+	* Changes animation if requested animation is not already current.
+	*/
+    animate: function(animation) {
+        if (this.bmp.currentAnimation.indexOf(animation) === -1 || this.isStopped) {
+			this.bmp.gotoAndPlay(animation);
+        }
+    }
 });
 
 factory['Player'] = Player;
